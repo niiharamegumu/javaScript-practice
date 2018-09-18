0f4b5943d8af034a341844a1286d2c1e58f3f21f
niiharamegumu <nhrmgm1223@gmail.com>
2018-09-18 21:02:46 +0900

    2018.09.18 commit.

--- game-trump-lesson/concentration/main.js
+++ game-trump-lesson/concentration/main.js
@@ -8,6 +8,7 @@ $(function(){
 		kind = ["c", "d", "h", "s"],
 		cards = [],
 		select_index = [],
+		select_kind = [],
 		select_num = "";
 
 
@@ -23,6 +24,7 @@ $(function(){
 		$li
 		.clone()
 		.data("num", cards[i].replace(/[^0-9]/g, ""))
+		.data("kind", cards[i].substring(0, 1))
 		.addClass("card is-surface")
 		.append(
 			$img
@@ -40,15 +42,42 @@ $(function(){
 	}
 
 	$table.on("click", "li", function(){
-		$(this).toggleClass("is-surface").toggleClass("is-reverse");
+		if(select_index.length >= 2){
+			return false;
+		}
 
+		$(this).toggleClass("is-surface").toggleClass("is-reverse");
 		if(select_num === ""){
 			select_num = $(this).data("num");
+			select_kind = $(this).data("kind");
 			select_index.push($(this).index());
-			console.log(select_num);
-			console.log(select_index);
+		} else {
+			if ($(this).index() !== select_index[0]){
+				select_index.push($(this).index());
+				if ($(this).data("num") === select_num){
+					console.log($(this).data("kind") + $(this).data("num") + ":" + select_kind + select_num + " →　○");
+					setTimeout(card_ok, 1500);
+				} else {
+					console.log($(this).data("kind") + $(this).data("num") + ":" + select_kind + select_num + " →　☓");
+					setTimeout(card_reverse, 1500);
+				}
+			}
 		}
 	});
+
+	function card_ok() {
+		$table.find("li").eq(select_index[0]).addClass("hit");
+		$table.find("li").eq(select_index[1]).addClass("hit");
+		select_num = "";
+		select_index = [];
+	}
+
+	function card_reverse() {
+		$table.find("li").eq(select_index[0]).toggleClass("is-surface").toggleClass("is-reverse");
+		$table.find("li").eq(select_index[1]).toggleClass("is-surface").toggleClass("is-reverse");
+		select_num = "";
+		select_index = [];
+	}
 
 	function shuffle(){
 		var
