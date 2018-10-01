4beb70839afda9f83e343e02939dd308ca28bb1d
niiharamegumu <nhrmgm1223@gmail.com>
2018-10-01 19:03:31 +0900

    It is possible to calculate once.

--- calculator/main.js
+++ calculator/main.js
@@ -3,27 +3,23 @@
 
 	var
 		$num = document.getElementsByClassName("num"),
+		$calculation = document.getElementsByClassName("calculation"),
 		$total = document.getElementById("total"),
 		$clear = document.getElementById("clear"),
-		$division = document.getElementById("division"),
-		$multiplication = document.getElementById("multiplication"),
-		$subtraction = document.getElementById("subtraction"),
-		$addition = document.getElementById("addition"),
 		$point = document.getElementById("point"),
 		$equal = document.getElementById("equal"),
 		total_string = "",
 		total_num = [],
 		calculation = false,
-		$num_length = $num.length;
+		calculation_type = "",
+		result,
+		$num_len = $num.length,
+		$calculation_len = $calculation.length;
 
 
-	for(var i = 0; i < $num_length; i++){
+
+	for(var i = 0; i < $num_len; i++){
 		$num[i].onclick = function(){
-			if(calculation){
-				calculation = false;
-				total_string = "";
-				$addition.classList.remove("calculation-button");
-			}
 			$total.textContent = this.textContent;
 			total_string += $total.textContent;
 			$total.textContent = total_string;
@@ -31,28 +27,49 @@
 	}
 
 
-	$addition.onclick = function(){
-		if(total_string){
-			calculation = true;
-			this.classList.add("calculation-button");
-			total_num.push(Number.parseInt(total_string, 10));
-		}
-	};
-
+	for(i = 0; i < $calculation_len; i++){
+		$calculation[i].onclick = function(){
+			set_array();
+			if(total_num.length == 1){
+				calculation = true;
+				calculation_type = this.textContent;
+			}
+		};
+	}
 
 	$equal.onclick = function(){
-		total_num.push(Number.parseInt(total_string, 10));
-		total_string = "";
-		total_string = String(total_num[0] + total_num[1]);
-		$total.textContent = total_string;
-		console.log(typeof total_string);
-	};
+		if(total_num.length == 1 && calculation){
+			set_array();
+		}
+		switch(calculation_type){
+			case "+" :
+				result = total_num[0] + total_num[1];
+				break;
+			case "-" :
+				result = total_num[0] - total_num[1];
+				break;
+			case "×" :
+				result = total_num[0] * total_num[1];
+				break;
+			case "÷" :
+				result = total_num[0] / total_num[1];
+				break;
+		}
+			$total.textContent = result;
+			calculation = false;
+		};
 
 
 	$clear.onclick = function(){
 		total_string = "";
 		total_num = [];
 		calculation = false;
+		calculation_type = "";
 		$total.textContent = "0";
 	};
+
+	function set_array(){
+		total_num.push(Number.parseInt(total_string, 10));
+		total_string = "";
+	}
 }());
