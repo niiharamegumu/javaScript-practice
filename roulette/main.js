f42fadabbfd333267c2f0a6e0c9d109ab09c1075
niiharamegumu <nhrmgm1223@gmail.com>
2018-12-14 18:28:20 +0900

    Fix.

--- roulette/main.js
+++ roulette/main.js
@@ -14,61 +14,74 @@ $(function(){
     num,
     result;
 
+
+
   for(var i = 1; i <= max; i++) {
-    bingo.push(i);
-    $number.append($("<li>").text(i));
-  }
+		bingo.push(i);
+		$number.append($("<li>").text(i));
+	}
 
   $stop.prop('disabled', true);
-  $reset.prop('disabled', true);
+
 
   $start.on('click', function () {
-    $start.prop('disabled', true);
-    $stop.prop('disabled', false);
+    set_start_disabled();
 
-    timer = setInterval(function(){
-      random = Math.floor( Math.random() * bingo.length );
-      num = bingo[random];
+    timer = setInterval(function() {
+      random = get_random( bingo.length );
+      result = bingo[random];
       $number
-        .find("li")
-        .removeClass("random")
-        .eq(parseInt(num, 10) - 1)
-        .addClass("random");
+				.find("li")
+				.removeClass("random")
+				.eq(parseInt(result, 10) - 1)
+				.addClass("random");
     }, 10);
 
   });
 
   $stop.on('click', function(){
-    $stop.prop('disabled', true);
-    $start.prop('disabled', false);
+    set_stop_disabled();
 
     clearInterval(timer);
-    result = bingo[random];
     bingo.splice(random, 1);
     $number
-      .find("li")
-      .eq(parseInt(result, 10) - 1)
-      .addClass("hit");
+			.find("li")
+			.eq(parseInt(result, 10) - 1)
+			.addClass("hit");
 
     bingo_len = bingo.length;
     if ( bingo_len === 0 ) {
-      $start.prop('disabled', true);
-      $stop.prop('disabled', true);
-      $reset.prop('disabled', false);
+        set_disabled();
     }
   });
 
   $reset.click(function(){
+    set_stop_disabled();
+    clearInterval(timer);
+    bingo = [];
     for(var i = 1; i <= max; i++) {
-      bingo.push(i);
-    }
+			bingo.push(i);
+		}
+	$number
+			.find("li")
+			.removeClass("random hit");
+  });
+
+
+  function get_random ( num ) {
+     return Math.floor( Math.random() * num );
+  }
+  function set_disabled () {
+    $start.prop('disabled', true);
+    $stop.prop('disabled', true);
+  }
+  function set_start_disabled () {
+    $start.prop('disabled', true);
+    $stop.prop('disabled', false);
+  }
+  function set_stop_disabled () {
     $start.prop('disabled', false);
     $stop.prop('disabled', true);
-    $reset.prop('disabled', true);
-    $number
-      .find("li")
-      .removeClass("random")
-      .removeClass("hit");
-  });
+  }
 
 });
