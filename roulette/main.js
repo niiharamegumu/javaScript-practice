5de4aa3424f1245e6675317e501816b7d66d3264
niiharamegumu <nhrmgm1223@gmail.com>
2018-12-30 11:04:03 +0900

    Fix function.

--- roulette/main.js
+++ roulette/main.js
@@ -17,71 +17,63 @@ $(function(){
 
 
   for(var i = 1; i <= max; i++) {
-		bingo.push(i);
-		$number.append($("<li>").text(i));
-	}
+    bingo.push(i);
+    $number.append($("<li>").text(i));
+  }
 
   $stop.prop('disabled', true);
 
 
   $start.on('click', function () {
-    set_start_disabled();
+    set_disabled( true, false );
 
     timer = setInterval(function() {
       random = get_random( bingo.length );
       result = bingo[random];
       $number
-				.find("li")
-				.removeClass("random")
-				.eq(parseInt(result, 10) - 1)
-				.addClass("random");
+        .find("li")
+        .removeClass("random")
+        .eq(parseInt(result, 10) - 1)
+        .addClass("random");
     }, 10);
 
   });
 
   $stop.on('click', function(){
-    set_stop_disabled();
+    set_disabled( false, true );
 
     clearInterval(timer);
     bingo.splice(random, 1);
     $number
-			.find("li")
-			.eq(parseInt(result, 10) - 1)
-			.addClass("hit");
+      .find("li")
+      .eq(parseInt(result, 10) - 1)
+      .addClass("hit");
 
     bingo_len = bingo.length;
     if ( bingo_len === 0 ) {
-        set_disabled();
+        set_disabled( true, true );
     }
   });
 
   $reset.click(function(){
-    set_stop_disabled();
+    set_disabled( false, true );
     clearInterval(timer);
     bingo = [];
     for(var i = 1; i <= max; i++) {
-			bingo.push(i);
-		}
-	$number
-			.find("li")
-			.removeClass("random hit");
+      bingo.push(i);
+    }
+  $number
+    .find("li")
+    .removeClass("random hit");
   });
 
 
   function get_random ( num ) {
-     return Math.floor( Math.random() * num );
+    return Math.floor( Math.random() * num );
   }
-  function set_disabled () {
-    $start.prop('disabled', true);
-    $stop.prop('disabled', true);
-  }
-  function set_start_disabled () {
-    $start.prop('disabled', true);
-    $stop.prop('disabled', false);
-  }
-  function set_stop_disabled () {
-    $start.prop('disabled', false);
-    $stop.prop('disabled', true);
+  function set_disabled ( startFlag, stopFlag ) {
+    $start.prop('disabled', startFlag);
+    $stop.prop('disabled', stopFlag);
   }
 
 });
